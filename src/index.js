caa47603c4eb75e47a64c83bf986ea24cca787e1
Latira <l@g.com>
2023-02-18 08:03:35 +0300

    init: upload final version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n=Math.abs(n);
+    let result = n % 10;
+    n = Math.floor(n / 10);
+    while (n >= 1) {
+      result = result*10 + n % 10;
+      n = Math.floor(n / 10);
+    }
+    return result;
 }
